0cd7894e5bc1745c0cc2080eae9b10d4e5ae832c
Marrek Nožka <marek@tlapicka.net>
2021-01-07 13:43:53 +0100

    modified:   requirements.txt     modified:   uzivatel.py

--- uzivatel.py
+++ uzivatel.py
@@ -1,9 +1,27 @@
 from pony.orm import db_session
-from webface.models import Uzivatel
+import random
+import string
+from webface.models import User, Shortener
 from werkzeug.security import generate_password_hash, check_password_hash
 
 login = input('zadej jmeno > ')
 heslo = input('zadej heslo > ')
 
 with db_session:
-    uzivatel1 = Uzivatel(login=login, heslo=generate_password_hash(heslo))
+    uzivatel = User.get(login=login)
+    if not uzivatel:
+        uzivatel = User(login=login, password=generate_password_hash(heslo))
+
+    while True:
+        adresa = input('zadej adresu >')
+        if adresa == '':
+            break
+        shortcut = "".join([random.choice(string.ascii_letters) for i in range(7)]) 
+        shortener = Shortener(shortcut=shortcut, url=adresa, user=uzivatel)
+        
+    for adresa in uzivatel.addresses:
+        print(adresa.shortcut, adresa.url)
+
+    adresy = tuple(uzivatel.addresses) 
+    for adresa in adresy:
+        print(adresa.shortcut, adresa.url)
